86bcc6f1cd7f009f6e8bd2066b6ffe7d76f339b0
jli <coderguy50@gmail.com>
2022-10-31 20:10:47 -0500

    added gitignore and added keybinds to interact with visualizer

--- integral-frag.glsl
+++ integral-frag.glsl
@@ -1,22 +1,32 @@
 #version 430
 
+#define LEFT 0
+#define RIGHT 1
+#define MID 2
+#define TRAPEZOID 3
+#define HIDE 4
+
 in vec4 fragCoord;
 
 //Demonstrates the left rectangle approximation of a Riemann Sum
 
 //Change this to change the interval
-#define INTERVAL 0.5
+uniform float uInterval = 0.5;
 //Change this change the max
-#define MAX 3.0
+uniform float uMax = 20.0;
 //Change this to change the min
-#define MIN -2.0
+uniform float uMin = -20.0;
 //Scale view
-#define SCALE 80.0
+uniform float uScale = 40.0;
+//center of the screen
+uniform vec2 uCenter = vec2(0.0, 0.0);
+
+uniform int uApproximationMethod = LEFT;
 
 //Change this to change the function
 float f(float x)
 {
-	return x * x + 2.0;
+	return sin(x) * x;
 }
 
 out vec4 fragColor;
@@ -25,20 +35,78 @@ void main()
 {
 	fragColor = vec4(0.0, 0.0, 0.0, 1.0);
 
-	vec2 trans = (fragCoord.xy) * 1.0 / SCALE;
+	vec2 trans = (fragCoord.xy) * 1.0 / uScale + uCenter;
     fragColor = vec4(0.0, 0.0, 0.0, 1.0);
-    if(trans.x < MAX && trans.x > MIN)
+    if(trans.x < uMax && trans.x > uMin)
+	{
         if(f(trans.x) > 0.0 && trans.y > 0.0 && f(trans.x) > trans.y)
-            fragColor += vec4(0.0, 1.0, 0.0, 1.0);
+            fragColor = vec4(0.0, 0.8, 0.0, 1.0);
         else if(f(trans.x) < 0.0 && trans.y < 0.0 && f(trans.x) < trans.y)
-            fragColor += vec4(0.0, 1.0, 0.0, 1.0);
-    
-    //Check if it's within the left rectangle
-    float x = floor(trans.x / INTERVAL) * INTERVAL;
-    if(trans.x < MAX && trans.x > MIN)
-        if(f(trans.x) > 0.0 && trans.y > 0.0 && f(x) > trans.y)
-            fragColor += vec4(1.0, 0.0, 0.0, 1.0);
-        else if(f(trans.x) < 0.0 && trans.y < 0.0 && f(x) < trans.y)
-            fragColor += vec4(1.0, 0.0, 0.0, 1.0);
+            fragColor = vec4(0.0, 0.8, 0.0, 1.0);
+	} 
 
+	float x;
+	float x1, x2;
+	float y;	
+	switch(uApproximationMethod)
+	{
+	case LEFT:
+		//Check if it's within the left rectangle
+    	x = floor(trans.x / uInterval) * uInterval;
+    	if(trans.x < uMax && trans.x > uMin)
+		{ 
+			if(f(trans.x) > 0.0 && trans.y > 0.0 && f(x) > trans.y)
+    	        fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    	    else if(f(trans.x) < 0.0 && trans.y < 0.0 && f(x) < trans.y)
+    	        fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+		}
+		break;
+	case RIGHT:
+		//Check if it's within the right rectangle
+    	x = ceil(trans.x / uInterval) * uInterval;
+    	if(trans.x < uMax && trans.x > uMin)
+		{ 
+			if(f(trans.x) > 0.0 && trans.y > 0.0 && f(x) > trans.y)
+    	        fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    	    else if(f(trans.x) < 0.0 && trans.y < 0.0 && f(x) < trans.y)
+    	        fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+		}		
+		break;
+	case MID:
+		//Check if it's within the rectangle
+    	x1 = floor(trans.x / uInterval) * uInterval;		
+    	x2 = ceil(trans.x / uInterval) * uInterval;
+		if(trans.x < uMax && trans.x > uMin)
+		{ 
+			if(f(trans.x) > 0.0 && trans.y > 0.0 && f((x1 + x2) * 0.5) > trans.y)
+    	        fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    	    else if(f(trans.x) < 0.0 && trans.y < 0.0 && f((x1 + x2) * 0.5) < trans.y)
+    	        fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+		}		
+		break;
+	case TRAPEZOID:
+		//Check if it's within the trapezoid
+    	x1 = floor(trans.x / uInterval) * uInterval;		
+    	x2 = ceil(trans.x / uInterval) * uInterval;
+		y = (f(x2) - f(x1)) / (x2 - x1) * (trans.x - x1) + f(x1);
+		if(trans.x < uMax && trans.x > uMin)
+		{ 
+			if(f(trans.x) > 0.0 && trans.y > 0.0 && y > trans.y)
+    	        fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    	    else if(f(trans.x) < 0.0 && trans.y < 0.0 && y < trans.y)
+    	        fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+		}	
+	default:
+		break;
+	}
+	
+	//Draw axes
+	if(abs(trans.x) <= 2.0 * 1.0 / uScale || abs(trans.y) <= 2.0 * 1.0 / uScale)
+		fragColor = vec4(1.0, 1.0, 1.0, 1.0);
+
+	//Draw line tickers
+	if(abs(trans.x - round(trans.x)) <= 2.0 * 1.0 / uScale && abs(trans.y) <= 8.0 * 1.0 / uScale)
+		fragColor = vec4(1.0, 1.0, 1.0, 1.0);
+	if(abs(trans.y - round(trans.y)) <= 2.0 * 1.0 / uScale && abs(trans.x) <= 8.0 * 1.0 / uScale)
+		fragColor = vec4(1.0, 1.0, 1.0, 1.0);
 }
--- main.c
+++ main.c
@@ -5,6 +5,15 @@
 
 #define MAX_SHADER_SIZE 8192
 
+enum
+{
+	LEFT,
+	RIGHT,
+	MID,
+	TRAPEZOID,
+	HIDE
+};
+
 const float rectangle[] = 
 {
 	1.0f, 1.0f,
@@ -17,7 +26,34 @@ const float rectangle[] =
 };
 
 unsigned int program;
+//Uniforms
 int winSizeLoc;
+
+int intervalLoc;
+#define MIN_INTERVAL 0.01f
+float interval = 0.5f;
+#define CHANGE_SPEED 0.001f
+float intervalChange = 0.0f;
+
+int scaleLoc;
+float scale = 40.0f;
+#define SCALE_UP 1.1
+#define MAX_SCALE 400.0f
+#define MIN_SCALE 10.0f
+
+int minLoc, maxLoc;
+float minVal = -20.0f, maxVal = 20.0f;
+float minValChange = 0.0f, maxValChange = 0.0f;
+#define SPEED 0.3f
+
+int approximationMethodLoc;
+int method = LEFT;
+
+int centerLoc;
+float centerX = 0.0f, centerY = 0.0f;
+float changeX = 0.0f, changeY = 0.0f;
+#define MOVE_SPEED 15.0f
+
 void setupProgram()
 {
 	program = glCreateProgram();	
@@ -88,6 +124,95 @@ void setupProgram()
 void handleWinResize(GLFWwindow *win, int newWidth, int newHeight)
 {
 	glViewport(0, 0, newWidth, newHeight);
+	glUniform2f(winSizeLoc, (float)newWidth, (float)newHeight);
+}
+
+//handle key input
+void handleKeyInput(GLFWwindow *win, int key, int scancode, int action, int mods)
+{
+	if(key == GLFW_KEY_I && action == GLFW_PRESS)
+		intervalChange = CHANGE_SPEED;
+	else if(key == GLFW_KEY_D && action == GLFW_PRESS)
+		intervalChange = -CHANGE_SPEED;
+
+	//CTRL + increases scale
+	if(key == GLFW_KEY_EQUAL && mods == GLFW_MOD_CONTROL && action == GLFW_PRESS)
+	{
+		scale *= SCALE_UP;
+		if(scale >= MAX_SCALE)
+			scale = MAX_SCALE;
+		glUniform1f(scaleLoc, scale);	
+	}
+	//CTRL - decreases scale
+	else if(key == GLFW_KEY_MINUS && mods == GLFW_MOD_CONTROL && action == GLFW_PRESS)
+	{	
+		scale /= SCALE_UP;
+		if(scale <= MIN_SCALE)
+			scale = MIN_SCALE;
+		glUniform1f(scaleLoc, scale);	
+	}
+
+	if(key == GLFW_KEY_L && action == GLFW_PRESS)
+	{
+		maxValChange = SPEED;
+	}
+	else if(key == GLFW_KEY_K && action == GLFW_PRESS)
+	{	
+		maxValChange = -SPEED;
+	}
+	else if(key == GLFW_KEY_J && action == GLFW_PRESS)
+	{	
+		minValChange = SPEED;
+	}
+	else if(key == GLFW_KEY_H && action == GLFW_PRESS)
+	{
+		minValChange = -SPEED;
+	}
+
+	//Change method
+	switch(key)
+	{
+	case GLFW_KEY_1:
+		method = LEFT;
+		glUniform1i(approximationMethodLoc, method);
+		break;
+	case GLFW_KEY_2:
+		method = RIGHT;
+		glUniform1i(approximationMethodLoc, method);
+		break;
+	case GLFW_KEY_3:
+		method = MID;
+		glUniform1i(approximationMethodLoc, method);
+		break;
+	case GLFW_KEY_4:
+		method = TRAPEZOID;
+		glUniform1i(approximationMethodLoc, method);
+		break;
+	case GLFW_KEY_5:
+		method = HIDE;	
+		glUniform1i(approximationMethodLoc, method);
+		break;
+	}
+
+	//Move around with the w,a,s,d keys
+	if(key == GLFW_KEY_UP && action == GLFW_PRESS)
+		changeY = MOVE_SPEED;
+	else if(key == GLFW_KEY_DOWN && action == GLFW_PRESS)
+		changeY = -MOVE_SPEED;
+
+	if(key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+		changeX = -MOVE_SPEED;
+	else if(key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+		changeX = MOVE_SPEED;
+
+	if(action == GLFW_RELEASE)
+	{
+		intervalChange = 0.0f;	
+		maxValChange = 0.0f;
+		minValChange = 0.0f;
+		changeX = 0.0f;
+		changeY = 0.0f;
+	}
 }
 
 int main(void)
@@ -96,7 +221,8 @@ int main(void)
 	glfwInit();
 	GLFWwindow* win = glfwCreateWindow(800, 800, "Integral Visualization", NULL, NULL);
 	glfwMakeContextCurrent(win);
-	glfwSetWindowSizeCallback(win, handleWinResize);	
+	glfwSetWindowSizeCallback(win, handleWinResize);		
+	glfwSetKeyCallback(win, handleKeyInput);	
 	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
 	//Set up rectangle
@@ -110,6 +236,12 @@ int main(void)
 	setupProgram();
 	glUseProgram(program);
 	winSizeLoc = glGetUniformLocation(program, "uWinSize");
+	intervalLoc = glGetUniformLocation(program, "uInterval");
+	scaleLoc = glGetUniformLocation(program, "uScale");
+	minLoc = glGetUniformLocation(program, "uMin");
+	maxLoc = glGetUniformLocation(program, "uMax");
+	approximationMethodLoc = glGetUniformLocation(program, "uApproximationMethod");
+	centerLoc = glGetUniformLocation(program, "uCenter");
 
 	//Main loop
 	while(!glfwWindowShouldClose(win))
@@ -123,11 +255,29 @@ int main(void)
 		while((err = glGetError()) != GL_NO_ERROR)
 			fprintf(stderr, "OpenGL Error: %d\n", err);
 
-		//Constantly update the window size
-		int winW, winH;
-		glfwGetWindowSize(win, &winW, &winH);
-		glUniform2f(winSizeLoc, (float)winW, (float)winH);
-	
+		glUniform1f(program, (float)interval);
+
+		if(interval >= MIN_INTERVAL)
+			interval += intervalChange;
+		else if(interval < MIN_INTERVAL)
+			interval = 0.01f;
+
+		minVal += minValChange;
+		maxVal += maxValChange;
+
+		centerX += changeX / scale;
+		centerY += changeY / scale;
+		glUniform2f(centerLoc, centerX, centerY);
+		
+		if(maxVal < minVal + 1.0f)
+		{
+			minValChange = 0.0f;
+			maxValChange = 0.0f;
+		}
+
+		glUniform1f(minLoc, minVal);
+		glUniform1f(maxLoc, maxVal);
+
 		glfwSwapBuffers(win);
 		glfwPollEvents();
 	}
